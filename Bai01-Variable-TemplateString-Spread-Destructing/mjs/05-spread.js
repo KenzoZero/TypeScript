cf2026e3e72e6445eacec837e14c6376c344bb9f
huynvn010193 <hpn010193@gmail.com>
2017-11-17 23:50:09 +0700

    Done video Spread

---
+++ Bai01-Variable-TemplateString-Spread-Destructing/mjs/05-spread.js
@@ -0,0 +1,5 @@
+let courseWeb = ["PHP","Zend","WordPress"];
+let courseMobile = ["Android","IOS"];
+let course = ["HTML","jQuery",...courseWeb,"Window Phone",...courseMobile];
+
+console.log(course);
