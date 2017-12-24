f033a3d45eef773e4b0778a36ab0745f781c4947
HuyNVN <hpn010193@gmail.com>
2017-12-24 22:26:42 +0700

    Done clip c03 typescript 08: Kiểu dữ liệu Typle

---
+++ Bai02_TypeScript/mjs/03_variable/03-tuple.js
@@ -0,0 +1,4 @@
+// kiểu dữ liệu typle.
+let x;
+x = ["Hello", 10, true];
+console.log(x);
---
+++ Bai02_TypeScript/ts/03_variable/03-tuple.ts
@@ -0,0 +1,5 @@
+// kiểu dữ liệu typle.
+let x: [string, number,boolean];
+x = ["Hello",10,true];
+
+console.log(x);
