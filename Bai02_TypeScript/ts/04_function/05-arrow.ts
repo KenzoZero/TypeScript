0c1555accc62ccd7b52ebcfe563f121649e9f5b8
HuyNVN <hpn010193@gmail.com>
2017-12-29 15:22:44 +0700

    Done clip c03 typescript 20

---
+++ Bai02_TypeScript/ts/04_function/05-arrow.ts
@@ -0,0 +1,19 @@
+
+// function có i kiểu number, sau khi xử lý trả về 1 number.Bên trong phần định nghĩa của number
+let f1 = function(i : number) : number{return i * i}
+console.log(f1(2));
+
+let f2 = function(i : number) {return i * i}
+console.log(f2(3));
+
+// Sử dụng arrow funciton: Khi đề cập đến arrow function thì từ khóa function không còn nữa.
+let f3 = (i : number): number => {return i * i}
+console.log(f3(4));
+
+// Sử dụng arrow funciton : không nói kết quã trả về
+let f4 = (i : number) => {return i * i}
+console.log(f4(5));
+
+// Sử dụng arrow funciton : không có kiểu trả về và không có lệnh return
+let f5 = (i : number) => i*i
+console.log(f5(6));
