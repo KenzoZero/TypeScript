dcf637c560370774f2d0c7fcc3b25e6d86b205b7
huynvn010193 <hpn010193@gmail.com>
2017-12-31 14:13:20 +0700

    Done clip c03 typescript 29: Generic

---
+++ Bai02_TypeScript/ts/06_generic/01-basic.ts
@@ -0,0 +1,17 @@
+function showNumberInfo(x:number):number
+{
+	return x;
+}
+
+function showStringInfo(x:string):string
+{
+	return x;
+}
+
+function showInfo<T>(x : T):T
+{
+	return x;
+}
+
+showInfo<string>("abc");
+showInfo<boolean>(true);
